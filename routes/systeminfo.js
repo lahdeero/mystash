fa36c839e40e9bf20cc51fdffad38ff406767f0d
Eero Lahdenperä <eero.lahdenpera@gmail.com>
2018-11-25 01:25:33 +0200

    last build broke heroku...

--- routes/systeminfo.js
+++ routes/systeminfo.js
@@ -2,9 +2,10 @@ const systeminfoRouter = require('express').Router()
 const shell = require('shelljs')
 
 systeminfoRouter.get('/', async (req, res) => {
-  let version = []
-  version[0] = shell.exec('uname -a', { silent:true }).stdout
-  version[1] = shell.exec('/home/muistiinpanot/scripts/cpu-temp.sh', { silent:true }).stdout
+  // let version = []
+  // version[0] = shell.exec('uname -a', { silent:true }).stdout
+  // version[1] = shell.exec('/home/muistiinpanot/scripts/cpu-temp.sh', { silent:true }).stdout
+	let version = ['heroku', 'rullaa']
   res.json(version)
 
   // var child = shell.exec('some_long_running_process', {async:true});
