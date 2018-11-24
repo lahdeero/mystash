a811e8bd5012b1f60adf2d786a0e6e952178a47c
Eero Lahdenperä <eero.lahdenpera@gmail.com>
2018-11-25 01:11:58 +0200

    ok client has to connect at start..

--- db/index.js
+++ db/index.js
@@ -18,8 +18,10 @@ const { Client } = require('pg');
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: true,
+  ssl: true
 });
+
+client.connect();
 
 module.exports = {
   query: (text, params) => client.query(text, params),
