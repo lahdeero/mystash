dd082685cfee56b53e1d4746480fa4aadf4cf197
Eero Lahdenperä <eero.lahdenpera@gmail.com>
2018-11-25 00:31:22 +0200

    databasen URL laitettu oikein..

--- db/index.js
+++ db/index.js
@@ -3,20 +3,28 @@ const pg = require('pg')
 
 require('dotenv').config()
 
-const config = {
-  user: process.env.PGUSER,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
-  max: 10, // max number of connection can be open to database
-  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-}
+// const config = {
+//   user: process.env.PGUSER,
+//   database: process.env.PGDATABASE,
+//   password: process.env.PGPASSWORD,
+//   port: process.env.PGPORT,
+//   max: 10, // max number of connection can be open to database
+//   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+// }
 
-const pool = new pg.Pool(config)
+// const pool = new pg.Pool(config)
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
 
 module.exports = {
-  query: (text, params) => pool.query(text, params),
-  connect: () => pool.connect(),
-  release: () => pool.end()
+  query: (text, params) => client.query(text, params),
+  connect: () => client.connect(),
+  release: () => client.end()
 }
-
