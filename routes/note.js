d8c1c8f3106b3e8777c7062820d0b63043ee2dcd
Eero Lahdenperä <eero.lahdenpera@gmail.com>
2019-01-04 07:54:59 +0200

    tables have english names now

--- routes/note.js
+++ routes/note.js
@@ -4,7 +4,7 @@ const client = require('../db')
 
 noteRouter.get('/all', async (req, res) => {
 	try {
-		const { rows } = await client.query('SELECT muistiinpano.id, otsikko, sisalto, array_agg(tagi.nimi) as tagit FROM muistiinpano LEFT JOIN muistiinpanotagi ON muistiinpanotagi.muistiinpano_id = muistiinpano.id LEFT JOIN tagi ON tagi.id = muistiinpanotagi.tagi_id GROUP BY muistiinpano.id, otsikko, sisalto ORDER BY muistiinpano.id DESC')
+    const { rows } = await client.query('SELECT note.id, title, content, array_agg(tag.name) as tags FROM note LEFT JOIN notetag ON notetag.note_id = note.id LEFT JOIN tag ON tag.id = notetag.tag_id GROUP BY note.id, title, content ORDER BY note.id DESC')
   	res.json(rows)
 	} catch (exception) {
 		res.send(exception)
@@ -14,7 +14,7 @@ noteRouter.get('/all', async (req, res) => {
 noteRouter.get('/search/:keyword', async (req, res) => {
 	const keyword = req.params.keyword
 	try {
-		const { rows } = await client.query('SELECT muistiinpano.id, otsikko, sisalto, array_agg(tagi.nimi) as tagit FROM muistiinpano LEFT JOIN muistiinpanotagi ON muistiinpanotagi.muistiinpano_id = muistiinpano.id LEFT JOIN tagi ON tagi.id = muistiinpanotagi.tagi_id WHERE tagi.nimi LIKE ($1) GROUP BY muistiinpano.id, otsikko, sisalto ORDER BY muistiinpano.id DESC', [keyword])
+		const { rows } = await client.query('SELECT note.id, title, content, array_agg(tag.name) as tags FROM note LEFT JOIN notetag ON notetag.note_id = note.id LEFT JOIN tag ON tag.id = notetag.tag_id WHERE tag.name LIKE ($1) GROUP BY note.id, title, content ORDER BY note.id DESC', [keyword])
 		res.json(rows)
 	} catch (exception) {
 		res.send(exception)
@@ -24,7 +24,7 @@ noteRouter.get('/search/:keyword', async (req, res) => {
 noteRouter.get('/note/:id', async (req, res) => {
 	const id = parseInt(req.params.id)
 	if (id === undefined || !Number.isInteger(id)) res.status(400).send('No id')
-	const { rows } = await client.query('SELECT muistiinpano.id, otsikko, sisalto, array_agg(tagi.nimi) as tagit FROM muistiinpano LEFT JOIN muistiinpanotagi ON muistiinpanotagi.muistiinpano_id = muistiinpano.id LEFT JOIN tagi ON tagi.id = muistiinpanotagi.tagi_id WHERE muistiinpano.id=($1) GROUP BY muistiinpano.id', [id])
+	const { rows } = await client.query('SELECT note.id, title, content, array_agg(tag.name) as tags FROM note LEFT JOIN notetag ON notetag.note_id = note.id LEFT JOIN tag ON tag.id = notetag.tag_id WHERE note.id=($1) GROUP BY note.id', [id])
   res.json(rows)
 })
 
@@ -33,40 +33,40 @@ noteRouter.put('/note/:id', async (req, res) => {
 	if (id === undefined || !Number.isInteger(id)) return res.status(400).send('No id')
 
 	const body = req.body
-	if (body.otsikko === undefined || body.otsikko.length === 0) return res.status(400).json({ error: 'title missing' })
-	else if (body.sisalto === undefined) return res.status(400).json({ error: 'contetent missing' })
+	if (body.title === undefined || body.title.length === 0) return res.status(400).json({ error: 'title missing' })
+	else if (body.content === undefined) return res.status(400).json({ error: 'contetent missing' })
 
   try {
     client.query('BEGIN')
-    await client.query('UPDATE muistiinpano SET otsikko =($1), sisalto =($2) WHERE ID =($3)', [body.otsikko, body.sisalto, id])
+    await client.query('UPDATE note SET title =($1), content =($2) WHERE ID =($3)', [body.title, body.content, id])
 
-    const currentTags = await client.query('SELECT tagi.nimi, muistiinpanotagi.muistiinpano_id, muistiinpanotagi.tagi_id FROM muistiinpanotagi LEFT JOIN tagi ON tagi.id = muistiinpanotagi.tagi_id WHERE muistiinpanotagi.muistiinpano_id = ($1)', [id])
+    const currentTags = await client.query('SELECT tag.name, notetag.note_id, notetag.tag_id FROM notetag LEFT JOIN tag ON tag.id = notetag.tag_id WHERE notetag.note_id = ($1)', [id])
 
 		//Check if notetags needs to be deleted
-		await currentTags.rows.forEach( async (rivi) => {
-			if (body.tagit.includes(rivi.nimi)) return
-			await client.query('DELETE FROM muistiinpanotagi WHERE muistiinpano_id = ($1) AND tagi_id = ($2)', [id, rivi.tagi_id])
+		await currentTags.rows.forEach( async (row) => {
+			if (body.tags.includes(row.name)) return
+			await client.query('DELETE FROM notetag WHERE note_id = ($1) AND tag_id = ($2)', [id, row.tag_id])
 		})
 
 		//Add new notetags (and new tags if needed)
-    await body.tagit.forEach( async (tagi) => {
+    await body.tags.forEach( async (tag) => {
       let tagId = 43 //for some reason result doesnt include added tags..
-      const selectRes = await client.query('SELECT id FROM tagi WHERE nimi = ($1)', [tagi])
+      const selectRes = await client.query('SELECT id FROM tag WHERE name = ($1)', [tag])
       if (selectRes.rows[0] === undefined) {
-        const insertRes = await client.query('INSERT INTO tagi(nimi) VALUES($1) RETURNING id', [tagi])
+        const insertRes = await client.query('INSERT INTO tag(name) VALUES($1) RETURNING id', [tag])
         tagId = insertRes.rows[0].id
       } else if (selectRes.rows[0] !== undefined){
         tagId = selectRes.rows[0].id
       }
-      const checkIfExists = await client.query('SELECT * FROM muistiinpanotagi WHERE muistiinpano_id =($1) AND tagi_id =($2)', [id, tagId])
+      const checkIfExists = await client.query('SELECT * FROM notetag WHERE note_id =($1) AND tag_id =($2)', [id, tagId])
       if (checkIfExists.rows[0] !== undefined) return
-      const insertNoteTag = 'INSERT INTO muistiinpanotagi(muistiinpano_id, tagi_id) VALUES ($1, $2)'
+      const insertNoteTag = 'INSERT INTO notetag(note_id, tag_id) VALUES ($1, $2)'
       const insertNoteTagValues = [id, tagId]
       await client.query(insertNoteTag, insertNoteTagValues)
     }) 
     await client.query('COMMIT')
     /* FOR SOME REASON result DOESNT INCLUDE ADDED TAGS */
-    const result = await client.query('SELECT muistiinpano.id, otsikko, sisalto, array_agg(tagi.nimi) as tagit FROM muistiinpano LEFT JOIN muistiinpanotagi ON muistiinpanotagi.muistiinpano_id = muistiinpano.id LEFT JOIN tagi ON tagi.id = muistiinpanotagi.tagi_id WHERE muistiinpano.id=($1) GROUP BY muistiinpano.id', [id])
+    const result = await client.query('SELECT note.id, title, content, array_agg(tag.name) as tags FROM note LEFT JOIN notetag ON notetag.note_id = note.id LEFT JOIN tag ON tag.id = notetag.tag_id WHERE note.id=($1) GROUP BY note.id', [id])
     await res.json(result.rows)
   } catch (error) {
     client.query('ROLLBACK')
@@ -79,32 +79,32 @@ noteRouter.put('/note/:id', async (req, res) => {
 noteRouter.post('/', async (req, res) => {
   const body = req.body
 
-  if (body.otsikko === undefined) return res.status(400).json({ error: 'title missing' })
-  else if (body.sisalto === undefined) return res.status(400).json({ error: 'contetent missing' })
+  if (body.title === undefined) return res.status(400).json({ error: 'title missing' })
+  else if (body.content === undefined) return res.status(400).json({ error: 'contetent missing' })
 
   let id
   try {
     await client.query('BEGIN')
-    const { rows } = await client.query('INSERT INTO muistiinpano(otsikko,sisalto) VALUES($1, $2) RETURNING id', [body.otsikko, body.sisalto])
+    const { rows } = await client.query('INSERT INTO note(title,content) VALUES($1, $2) RETURNING id', [body.title, body.content])
     id = rows[0].id
 
-    await body.tagit.forEach( async (tagi) => {
+    await body.tags.forEach( async (tag) => {
       let tagId
-      const selectRes = await client.query('SELECT id FROM tagi WHERE nimi = ($1)', [tagi])
+      const selectRes = await client.query('SELECT id FROM tag WHERE name = ($1)', [tag])
       if (selectRes.rows[0] === undefined) {
-        const insertRes = await client.query('INSERT INTO tagi(nimi) VALUES($1) RETURNING id', [tagi])
+        const insertRes = await client.query('INSERT INTO tag(name) VALUES($1) RETURNING id', [tag])
         tagId = await insertRes.rows[0].id
       } else if (selectRes.rows[0] !== undefined){
         tagId = await selectRes.rows[0].id
       }
 
-      const insertNoteTag = await 'INSERT INTO muistiinpanotagi(muistiinpano_id, tagi_id) VALUES ($1, $2)'
+      const insertNoteTag = await 'INSERT INTO notetag(note_id, tag_id) VALUES ($1, $2)'
       const insertNoteTagValues = await [id, tagId]
       await client.query(insertNoteTag, insertNoteTagValues)
     })
     
     await client.query('COMMIT')
-    const result = await client.query('SELECT muistiinpano.id, otsikko, sisalto, array_agg(tagi.nimi) as tagit FROM muistiinpano LEFT JOIN muistiinpanotagi ON muistiinpanotagi.muistiinpano_id = muistiinpano.id LEFT JOIN tagi ON tagi.id = muistiinpanotagi.tagi_id WHERE muistiinpano.id=($1) GROUP BY muistiinpano.id', [id])
+    const result = await client.query('SELECT note.id, title, content, array_agg(tag.name) as tags FROM note LEFT JOIN notetag ON notetag.note_id = note.id LEFT JOIN tag ON tag.id = notetag.tag_id WHERE note.id=($1) GROUP BY note.id', [id])
     await res.json(result.rows)
   } catch (exception) {
     await client.query('ROLLBACK')
@@ -120,10 +120,10 @@ noteRouter.delete('/note/:id', async (req, res) => {
   let message = ''
   try {
     await client.query('BEGIN')
-    const { rows } = await client.query('SELECT * FROM muistiinpano WHERE id = ($1)', [id])
+    const { rows } = await client.query('SELECT * FROM note WHERE id = ($1)', [id])
     if (rows[0] !== undefined) {
-      message = await 'Note ' + rows[0].id + ' deleted! title: ' + rows[0].otsikko + ' content: ' + rows[0].sisalto
-      await client.query('DELETE FROM muistiinpano WHERE id = ($1)', [id])
+      message = await 'Note ' + rows[0].id + ' deleted! title: ' + rows[0].title + ' content: ' + rows[0].content
+      await client.query('DELETE FROM note WHERE id = ($1)', [id])
       await client.query('COMMIT')
     }
   } catch (exception)  {
