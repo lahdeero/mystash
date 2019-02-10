1848599db501328047c8eef3f2d5efcf468873ab
Eero Lahdenperä <eero.lahdenpera@gmail.com>
2019-02-10 23:55:08 +0200

    added jest and supertest to package.json

---
+++ tests/note_api.test.js
@@ -0,0 +1,20 @@
+var request = require('supertest');
+var express = require('express');
+
+var app = express();
+
+app.get('',function(req,res,next){
+  res.statusCode = 200;
+  res.end()
+});
+
+describe('app js test', function() {
+  describe('GET /', function() {
+     it('should respond to GET with empty path', function (done) {
+        request(app)
+               .get('')
+               .expect(200)
+               .end(done) 
+     });
+  });
+});
