3a322b960e2fc86fe85164e87679383361f22966
Eero Lahdenperä <eero.lahdenpera@gmail.com>
2019-02-16 23:24:21 +0200

    successful register prints error 'objects are not valid as react..' at handleRegister

--- src/client/src/components/Register.js
+++ src/client/src/components/Register.js
@@ -11,39 +11,54 @@ const Register = (props) => {
   const [email, setEmail] = useState('')
   const [error, setError] = useState('')
 
+  const validateInput = () => {
+    if (username.length < 3) {
+      setError('usename must be atleast 3 characters')
+      setTimeout(() => { setError('') }, 5000)
+      return false
+    } else if (password.length < 6) {
+      setError('password must be atleast 6 characters')
+      setTimeout(() => { setError('') }, 5000)
+      return false
+    }
+    return true
+  }
   const handleRegister = async (event) => {
     event.preventDefault()
-    try {
-      const tokenAndMessage = await props.actionForRegister({
-        realname: firstname + ' ' + lastname,
-        username: username,
-        password: password,
-        email: email
-      })
-      const user = tokenAndMessage[0]
-      await window.localStorage.setItem('loggedMystashappUser', JSON.stringify(user))
-      await props.setLogin(user)
-    } catch (exception) {
-      setError(exception)
-      setTimeout(() => {
-        setError(null)
-      }, 5000)
+    if (validateInput()) {
+      try {
+        const tokenAndMessage = await props.actionForRegister({
+          realname: firstname + ' ' + lastname,
+          username: username,
+          password: password,
+          email: email
+        })
+        const user = tokenAndMessage[0]
+        await window.localStorage.setItem('loggedMystashappUser', JSON.stringify(user))
+        await props.setLogin(user)
+      } catch (exception) {
+        setError(exception)
+        setTimeout(() => {
+          setError('')
+        }, 5000)
+      }
     }
   }
 
+  const errorMessage = error === '' ? null : error
   return (
     <div>
       <Navbar className="indigo" brand='my-stash' right>
       </Navbar>
       <div className="container">
-        <div>{error}</div>
+        {errorMessage ? <div className="error">{error}</div> : <div className="notification">{'(*) are required fields'}</div>}
         <div>
           <form onSubmit={handleRegister}>
             <Row>
-              <Input onChange={(event) => setFirstname(event.target.value)} name="firstname" label="First Name1" ><Icon>accessibility</Icon></Input>
-              <Input onChange={(event) => setLastname(event.target.value)} name="lastname" s={6} label="Second name" ><Icon>accessibility_new</Icon></Input>
-              <Input onChange={(event) => setUsername(event.target.value)} name="username" s={12} label="Username" ><Icon>account_circle</Icon></Input>
-              <Input onChange={(event) => setPassword(event.target.value)} name="password" type="password" label="password" s={12} ><Icon>https</Icon></Input>
+              <Input onChange={(event) => setFirstname(event.target.value)} name="firstname" label="First Name" ><Icon>accessibility</Icon></Input>
+              <Input onChange={(event) => setLastname(event.target.value)} name="lastname" s={6} label="Last name" ><Icon>accessibility_new</Icon></Input>
+              <Input onChange={(event) => setUsername(event.target.value)} name="username" s={12} label="Username(*)" ><Icon>account_circle</Icon></Input>
+              <Input onChange={(event) => setPassword(event.target.value)} name="password" type="password" label="Password(*)" s={12} ><Icon>https</Icon></Input>
               <Input onChange={(event) => setEmail(event.target.value)} name="email" type="email" label="Email" s={12} ><Icon>email</Icon></Input>
             </Row>
             <Button type="submit">Register</Button>
