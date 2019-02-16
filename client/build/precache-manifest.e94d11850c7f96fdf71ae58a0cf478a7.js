cd1f00593991d68244061bb8fc8a359c5ff1f3d7
Eero Lahdenperä <eero.lahdenpera@gmail.com>
2019-02-16 19:30:51 +0200

    there was no need to move build folder

---
+++ client/build/precache-manifest.e94d11850c7f96fdf71ae58a0cf478a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "10a2da8c24bfb180fc60",
+    "url": "/static/css/main.e379e771.chunk.css"
+  },
+  {
+    "revision": "10a2da8c24bfb180fc60",
+    "url": "/static/js/main.10a2da8c.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "11348367dc4b24681385",
+    "url": "/static/js/2.11348367.chunk.js"
+  },
+  {
+    "revision": "475d6600a171d8d9c7e11b47c0c1afb2",
+    "url": "/index.html"
+  }
+];
