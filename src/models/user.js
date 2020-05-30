f3463f1a98dd6af225c967962f0bb3e49bbf47ba
Eero Lahdenperä <eero.lahdenpera@gmail.com>
2020-05-30 17:19:07 +0300

    cleaner user router

--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,83 @@
-class User {
-  constructor(username = String, password, realname, tier) {
-    this.username = username
-    this.password = password
-    this.realname = realname
-    this.tier = tier
-  }
+const pool = require('../db')
+
+// class User {
+//   constructor(row) {
+//     this.id = row.id
+//     this.username = row.username
+//     this.realname = row.realname
+//     this.email = row.email
+//     this.tier = row.tier
+//   }
+// }
+
+createUserObject = (row) => {
+  return { id: row.id, username: row.username, realname: row.realname, email: row.email, tier: row.tier }
 }
+
+const findOne = (id) => {
+  const qry = `
+    SELECT id, username, realname, email, github_id 
+    FROM account
+    WHERE id=$1`
+  return pool
+    .query(qry, [id])
+    .then(res => {
+      return createUserObject(res.rows[0])
+    })
+    .catch(e => console.error(e.stack))
+}
+
+const findUserByGhProfile = (profile) => {
+  const qry = `
+    SELECT id, username, realname, email, tier 
+    FROM account
+    WHERE github_id=$1`
+  return pool
+    .query(qry, [profile.id])
+    .then(res => {
+      return res.rows[0]
+    })
+    .catch(e => console.error(e.stack))
+}
+
+const findOrCreateUser = (profile) => {
+  return findUserByGhProfile(profile).then(result => {
+    if (result) {
+      return createUserObject(result)
+    } else {
+      return createUser(profile).then(res => {
+        return createUserObject(res)
+      })
+    }
+  })
+}
+
+const createUser = (profile) => {
+  const displayName = profile.displayName ? profile.displayName : profile.username
+  const email = profile._json.email ? profile._json.email : `${displayName}@unknow.mail`
+  const query = `
+      INSERT INTO account 
+      (username,realname,email,github_id,tier,register_date)
+       VALUES ($1, $2, $3, $4, 1, NOW()) RETURNING *`
+  const values = [profile.username, displayName, email, profile.id]
+
+  return pool
+    .connect()
+    .then(client => {
+      return client
+        .query(query, values)
+        .then(res => {
+          client.release()
+          return res.rows[0]
+        })
+        .catch(err => {
+          client.release()
+          console.log(err.stack)
+        })
+    }).catch(err => {
+      console.log(err.stack)
+    })
+}
+// return client.query(query, [profile.username, displayName, email, profile.id])
+
+module.exports = { findOne, findUserByGhProfile, findOrCreateUser, createUser }
--- src/routes/login.js
+++ src/routes/login.js
@@ -4,115 +4,55 @@ const bcrypt = require('bcrypt')
 const pool = require('../db')
 const passport = require('passport')
 const GitHubStrategy = require('passport-github2').Strategy
+const User = require('../models/user')
 
 loginRouter.use(passport.initialize())
-
-passport.serializeUser(function (user, done) {
-  console.log('serialize')
-  done(null, user)
-})
 
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
   callbackURL: process.env.CALLBACK_URL,
 },
-  function (accessToken, refreshToken, profile, done) {
-    // User.findOrCreate({ githubId: profile.id }, function (err, user) {
-    //   return done(err, user)
-    // })
-    process.nextTick(function () {
-      // console.log('profile:', profile)
-      return done(null, profile)
-    })
-  }
-))
-
-loginRouter.get('/github',
-  passport.authenticate('github', { scope: ['user:email'] })
+  (accessToken, refreshToken, profile, done) => {
+    User.findOrCreateUser(profile)
+      .then(user => {
+        console.log('loggin user', user)
+        return done(null, user)
+      })
+      .catch(e => {
+        return (null, false, { error: 'No user found' })
+      })
+  })
 )
 
-const createUser = async (ghuser) => {
-  const today = new Date()
-  // console.log('passportuser', passportuser)
-  const { username, displayName, _json } = ghuser
-  console.log(username)
-  console.log(displayName)
-  console.log(_json.email)
-  console.log(1)
-  console.log(today)
-  const client = await pool.connect()
-  const { rows } = await client.query('INSERT INTO account (username,realname,email,github_id,tier,register_date) VALUES ($1, $2, $3, $4, 1, NOW()) RETURNING id',
-    [username, displayName, _json.email, ghuser.id])
-  client.release()
-  console.log('eioo')
-  const accountId = rows[0].id
-  console.log('accid', accountId)
-  const createduser = {
-    username: username,
-    realname: displayName,
-    id: accountId,
-    github_id: ghuser.id,
-    tier: 1,
-    email: _json.email
-  }
-  return createduser
-}
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
 
-const findOrCreateUser = async (ghuser) => {
-  const client = await pool.connect()
-  const res = await client.query('SELECT * FROM account WHERE github_id = ($1) LIMIT 1', [ghuser.id])
-  let data = res.rows[0]
-  // console.log('data', data)
+passport.deserializeUser((userId, done) => {
+  User.findOne(userId).then(user => {
+    done(null, user)
+  }).catch(err => done(err))
+})
 
-  if (!data) {
-    const foo = await client.query('SELECT * FROM account WHERE email = ($1) LIMIT 1', [ghuser._json.email])
-    // console.log('foo', foo.rows[0])
-    const findbyemail = foo.rows[0] ? foo.rows[0].email : null
-    // console.log('fbem', findbyemail)
-    if (findbyemail) {
-      await client.query('UPDATE account SET github_id = ($1) WHERE email = ($2)', [ghuser.id, findbyemail])[0]
-      const resagain = await client.query('SELECT * FROM account WHERE github_id = ($1) LIMIT 1', [ghuser.id])
-      data = resagain.rows[0]
-    }
-  }
 
-  const usermade = data ? {
-    username: data.username,
-    realname: data.realname,
-    id: data.id,
-    github_id: data.github_id,
-    tier: data.tier,
-    email: data.email
-  } : await createUser(ghuser)
-
-  console.log('usedmade', usermade)
-  client.release()
-
-  return usermade
-}
+loginRouter.get('/github',
+  passport.authenticate('github')
+)
 
 loginRouter.get('/github/callback',
   passport.authenticate('github', { failureRedirect: '/login' }),
-  async (req, res) => {
-    console.log('callback authentikaatio onnistui')
-    // console.log('request user', req.user)
-
+  (req, res) => {
     try {
-      // const passwordCorrect = data === null ? false : await bcrypt.compare(body.password, data.password)
-      const user = await findOrCreateUser(req.user)
-      const token = jwt.sign(user, process.env.SECRET)
-
-      console.log('successful login: ', user.username)
-      // res.redirect(process.env.NODE_ENV === 'dev' ? 'http://localhost:3000?token=' + token : process.env.FRONTEND_URL + '?token=' + token)
+      const token = jwt.sign(req.user, process.env.SECRET)
       const redirect_url = `${process.env.FRONTEND_URL}?token=${token}`
       res.redirect(redirect_url)
     } catch (exception) {
-      console.log('failed login, user: ' + req.user.displayName)
+      console.log('failed login, user: ' + req.user.username)
       return res.status(401).json({ error: 'github login failed' })
     }
-  })
-
+  }
+)
 
 loginRouter.post('/', async (request, response) => {
   const body = request.body
