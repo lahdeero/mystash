20709112627ca78b077fcad1b4a81a4067aa927d
Eero Lahdenperä <19709320+lahdeero@users.noreply.github.com>
2022-08-14 22:30:04 +0300

    Add frontned build to under public

---
+++ public/precache-manifest.0e5f2ba5dafab33971beab6d01373b3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f2d189de5cd2c3f4a15",
+    "url": "/mystash-frontend/static/css/main.dfceb77e.chunk.css"
+  },
+  {
+    "revision": "6f2d189de5cd2c3f4a15",
+    "url": "/mystash-frontend/static/js/main.6f2d189d.chunk.js"
+  },
+  {
+    "revision": "3d5cb9bfc4284c14249f",
+    "url": "/mystash-frontend/static/js/runtime~main.3d5cb9bf.js"
+  },
+  {
+    "revision": "5f37d9f6ef52bfa07e28",
+    "url": "/mystash-frontend/static/js/2.5f37d9f6.chunk.js"
+  },
+  {
+    "revision": "ca90075989462c2e377a296bbc937f37",
+    "url": "/mystash-frontend/static/js/2.5f37d9f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "788ad6188f696dd81eb3f8225b2ffbfd",
+    "url": "/mystash-frontend/index.html"
+  }
+];
--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const path = require('path')
 const client = require('./db')
 const noteRouter = require('./routes/note')
 const tagRouter = require('./routes/tag')
 const userRouter = require('./routes/user')
 const loginRouter = require('./routes/login')
 
-// const router = express.Router()
+app.use(express.static('public'))
+app.use('/mystash-frontend/static', express.static(path.join(__dirname, "../public/static")))
+console.log("__dirname", __dirname)
 app.use(bodyParser.json())
 app.use(cors())
 
@@ -19,11 +22,6 @@ app.use('/api/user', userRouter)
 app.use('/api/login', loginRouter)
 
 const server = http.createServer(app)
-
-app.get('/', function (_req, res) {
-  // res.send(`<a href="https://lahdeero.github.io/mystash-frontend/">frontend</a>`)
-  res.redirect('https://lahdeero.github.io/mystash-frontend/')
-})
 
 app.get('/ping', function (_req, res) {
   res.send(`<html><body>Hello from backend! Timestamp: ${Date()}</body></html>`)
