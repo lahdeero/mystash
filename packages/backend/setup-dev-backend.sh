55a08a6671326b4b3db87806ca517d52db830c06
Eero Lahdenperä <19709320+lahdeero@users.noreply.github.com>
2025-06-02 01:10:27 +0300

    Revert "Set DYNAMODB_ENDPOINT as variable"
    This reverts commit c6eceb09de552dd8277d5e92bd42496c51567c3f.

--- packages/backend/setup-dev-backend.sh
+++ packages/backend/setup-dev-backend.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-if [ -f .env ]; then
-    echo Setup environment variables...
-    source .env
-    export MYSTASH_SECRET
-    export GITHUB_CLIENT_ID
-    export GITHUB_CLIENT_SECRET
-    export GITHUB_REDIRECT_URI
-    export DYNAMODB_ENDPOINT
-else
-     echo "No .env file found, assuming environment variables are already set."
-fi
+# TODO: Logging to local-init.log doesnt work
 
 echo "Remove existing Docker containers..."
 docker rm -f dynamodb-local s3-local 2>/dev/null
@@ -187,3 +177,14 @@ aws dynamodb put-item \
         "updatedAt": {"S": "2024-10-13T17:30:31.222Z"}
     }' \
     --endpoint-url http://localhost:8001 > local-init.log 2>&1
+
+if [ -f .env ]; then
+    echo Setup environment variables...
+    source .env
+    export MYSTASH_SECRET
+    export GITHUB_CLIENT_ID
+    export GITHUB_CLIENT_SECRET
+    export GITHUB_REDIRECT_URI
+else
+     echo "No .env file found, assuming environment variables are already set."
+fi
