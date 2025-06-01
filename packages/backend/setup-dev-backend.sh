70ada537d833f17e705bdab90e6680e26d529dde
Eero Lahdenperä <19709320+lahdeero@users.noreply.github.com>
2025-06-02 01:41:09 +0300

    Add checks to setup-dev-backend.sh

--- packages/backend/setup-dev-backend.sh
+++ packages/backend/setup-dev-backend.sh
@@ -74,7 +74,7 @@ aws dynamodb create-table \
             }
         }]' \
     --billing-mode PAY_PER_REQUEST \
-    --endpoint-url http://localhost:8001 > local-init.log 2>&1
+    --endpoint-url http://localhost:8001
 
 echo "Create the notes table..."
 aws dynamodb create-table \
@@ -148,6 +148,16 @@ aws dynamodb create-table \
         }]' \
     --billing-mode PAY_PER_REQUEST \
     --endpoint-url http://localhost:8001 > local-init.log 2>&1
+
+echo "Waiting for mystash-dev-users table to be active..."
+aws dynamodb wait table-exists --table-name mystash-dev-users --endpoint-url http://localhost:8001
+
+echo "Waiting for mystash-dev-notes table to be active..."
+aws dynamodb wait table-exists --table-name mystash-dev-notes --endpoint-url http://localhost:8001
+
+echo "Waiting for mystash-dev-files table to be active..."
+aws dynamodb wait table-exists --table-name mystash-dev-files --endpoint-url http://localhost:8001
+
 
 echo "Seed the users table..."
 aws dynamodb put-item \
