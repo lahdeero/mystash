d770fbbea35cc96dbfb790203807d9316b8d6773
Eero Lahdenperä <19709320+lahdeero@users.noreply.github.com>
2025-06-02 00:01:47 +0300

    Add CI screenshot

--- packages/e2e/playwright.config.ts
+++ packages/e2e/playwright.config.ts
@@ -9,5 +9,7 @@ export default defineConfig({
     baseURL: 'http://localhost:3000',
     headless: true,
     viewport: { width: 1280, height: 720 },
+    screenshot: 'only-on-failure',
   },
+  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
 })
